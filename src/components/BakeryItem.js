eed9638cf165e7aac8b59546f7a9f4cc7609caac
michellel3 <michelle_r_lu@brown.edu>
2022-11-09 19:57:56 -0500

    added menu items

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,25 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+    const item = props.item;
+    return  (
+        <div className="BakeryItem">
+            <img src={item.image}/>
+            <div className="description">
+                <div className="name">
+                    {item.name}
+                </div>
+                <div className="desc">
+                    {item.description}
+                </div>
+                <div className="price">
+                    {item.price}
+                </div>
+                <div className="add">
+                    <button className="button">
+                        Add
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
